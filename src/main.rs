b97a91d770fb113b5d1bd4d1a188262202691d56
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-12-11 00:37:51 +0200

    Add input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Print the expression to calc. Using only 0-9+-/*^");
+    let mut expression = String::new();
+    io::stdin()
+    .read_line(&mut expression)
+    .expect("Failed to read input");
+    
+    println!("Entered string is {}", expression);
 }
